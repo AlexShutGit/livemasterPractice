4636885342ab6aba9c3fab4847443be6620eb188
sandmanitsu_ <trapchevy67@gmail.com>
2023-10-22 20:04:10 +0300

    Начал делать слайдер

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+
+
+
+const slider = (elem) => {
+    if (elem.target.id == 'prev-slide') {
+        slideIndex = slideIndex - 1;
+    } else {
+        slideIndex = slideIndex + 1;
+    }
+
+    if (slideIndex < 0) {
+        slideIndex = 0;
+    }
+
+    if (slideIndex > 2) {
+        slideIndex = 2;
+    }
+    console.log(slideIndex);
+
+    const slides = document.querySelectorAll('.reviews__slider');
+    const currSlide = document.getElementById(slideIndex); 
+
+    for (let i = 0; i < slides.length; i++) {
+        if (slides[i].id == slideIndex) {   
+            slides[i].setAttribute('class', 'reviews__slider');
+        } else {
+            slides[i].setAttribute('class', 'reviews__slider slider-hide');
+        }
+    }   
+}
+
+
+
+
+
 const initPageLogic = () => {
 
     const header = document.getElementById('header');
@@ -10,7 +45,13 @@ const initPageLogic = () => {
             header.setAttribute('class','header__bar')
         }
     })
+
+    const prevButton = document.getElementById('prev-slide');
+    const nextButton = document.getElementById('next-slide');
+    prevButton.addEventListener('click', slider)
+    nextButton.addEventListener('click', slider)
 }
 
-document.addEventListener('DOMContentLoaded', initPageLogic);
+let slideIndex = 0;
+document.addEventListener('DOMContentLoaded', initPageLogic,);
 
