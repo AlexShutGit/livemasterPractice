83d9916ced369894cc9863437a77e298d892eb8f
trapchevy67-gmail-com <trapchevy67@gmail.com>
2023-10-23 17:24:09 +0300

    header + slider полурабочий

--- js/main.js
+++ js/main.js
@@ -12,26 +12,79 @@ const slider = (elem) => {
         slideIndex = 0;
     }
 
-    if (slideIndex > 2) {
-        slideIndex = 2;
+    if (slideIndex > 4) {
+        slideIndex = 4;
     }
     console.log(slideIndex);
 
     const slides = document.querySelectorAll('.reviews__slider');
-    const currSlide = document.getElementById(slideIndex); 
+    const cirles = document.querySelectorAll('.reviews__slider-bar-checkboxes-circle');
 
     for (let i = 0; i < slides.length; i++) {
         if (slides[i].id == slideIndex) {   
             slides[i].setAttribute('class', 'reviews__slider');
+            cirles[i].setAttribute('class', 'reviews__slider-bar-checkboxes-circle checked');
         } else {
             slides[i].setAttribute('class', 'reviews__slider slider-hide');
+            cirles[i].setAttribute('class', 'reviews__slider-bar-checkboxes-circle');
         }
     }   
 }
 
+const touchStart = (event) => {
+    xDown = event.touches[0].clientX;
+    yDown = event.touches[0].clientY;
+}
 
+const touchMove = (event) => {
+    if (!xDown || !yDown) {
+        return;
+    }
+    
+    let xUp = event.touches[0].clientX;
+    let yUp = event.touches[0].clientY;
 
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
 
+    const slides = document.querySelectorAll('.reviews__slider');
+    const cirles = document.querySelectorAll('.reviews__slider-bar-checkboxes-circle');
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0) {
+            console.log('left swipe');
+            slideIndex = slideIndex + 1;
+
+            if (slideIndex > 4) {
+                slideIndex = 4;
+            }
+
+            console.log(slideIndex);
+            
+        } else {
+            console.log('right swipe');
+            slideIndex = slideIndex - 1;
+            
+            if (slideIndex < 0) {
+                slideIndex = 0;
+            }
+            console.log(slideIndex);
+        }
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        if (slides[i].id == slideIndex) {   
+            slides[i].setAttribute('class', 'reviews__slider');
+            cirles[i].setAttribute('class', 'reviews__slider-bar-checkboxes-circle checked');
+        } else {
+            slides[i].setAttribute('class', 'reviews__slider slider-hide');
+            cirles[i].setAttribute('class', 'reviews__slider-bar-checkboxes-circle');
+        }
+    } 
+
+    xDown = null;
+    yDown = null;
+}
 
 const initPageLogic = () => {
 
@@ -50,8 +103,16 @@ const initPageLogic = () => {
     const nextButton = document.getElementById('next-slide');
     prevButton.addEventListener('click', slider)
     nextButton.addEventListener('click', slider)
+
+    if (window.innerWidth < 769) {
+        document.addEventListener('touchstart', touchStart);
+        document.addEventListener('touchmove', touchMove);
+    }
 }
 
 let slideIndex = 0;
+let xDown = null;
+let yDown = null;
+
 document.addEventListener('DOMContentLoaded', initPageLogic,);
 
